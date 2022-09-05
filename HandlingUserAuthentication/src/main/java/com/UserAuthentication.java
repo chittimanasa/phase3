03a681ccfabe43dba693aae9584064072001b7e7
chittimanasa <chittimanasa29@gmail.com>
2022-09-05 10:37:24 +0530

    second

---
+++ HandlingUserAuthentication/src/main/java/com/UserAuthentication.java
@@ -0,0 +1,16 @@
+package com;
+
+public class UserAuthentication {
+	public String getUserName() {
+		return "manasa";
+	}
+	public String getEmail() {
+		return "manasachitti@gmail.com";
+	}
+	public int getPhnNum() {
+		return 949312788;
+	}
+	public String getPassword() {
+		return "manu@123";
+	}
+}
